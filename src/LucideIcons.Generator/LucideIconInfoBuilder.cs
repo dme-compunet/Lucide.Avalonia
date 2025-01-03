97c0c1b52eebf8804184db8ad32a176d5f9ee2af
dme-compunet <117437050+dme-compunet@users.noreply.github.com>
2025-01-03 11:15:57 +0200

    Update LucideIconInfoBuilder.cs

--- src/LucideIcons.Generator/LucideIconInfoBuilder.cs
+++ src/LucideIcons.Generator/LucideIconInfoBuilder.cs
@@ -29,50 +29,116 @@ public class LucideIconInfoBuilder
     {
         var sb = new StringBuilder();
 
-        sb.AppendLine("namespace Lucide.Avalonia.Metadata;");
-        sb.AppendLine();
-        sb.AppendLine("public class LucideIconInfo");
-        sb.AppendLine("{");
-        sb.AppendLine("    public required LucideIconKind Kind { get; init; }");
-        sb.AppendLine();
-        sb.AppendLine("    public required string[] Contributors { get; init; }");
-        sb.AppendLine();
-        sb.AppendLine("    public required string[] Categories { get; init; }");
-        sb.AppendLine();
-        sb.AppendLine("    public required string[] Tags { get; init; }");
-        sb.AppendLine();
-        sb.AppendLine("    public bool Contains(string? value)");
-        sb.AppendLine("    {");
-        sb.AppendLine("        if (string.IsNullOrEmpty(value))");
-        sb.AppendLine("        {");
-        sb.AppendLine("            return true;");
-        sb.AppendLine("        }");
-        sb.AppendLine();
-        sb.AppendLine("        value = value.ToLower();");
-        sb.AppendLine();
-        sb.AppendLine("        if (Kind.ToString().Contains(value, StringComparison.OrdinalIgnoreCase))");
-        sb.AppendLine("        {");
-        sb.AppendLine("            return true;");
-        sb.AppendLine("        }");
-        sb.AppendLine();
-        sb.AppendLine("        foreach (var category in Categories)");
-        sb.AppendLine("        {");
-        sb.AppendLine("            if (category.Contains(value))");
-        sb.AppendLine("            {");
-        sb.AppendLine("                return true;");
-        sb.AppendLine("            }");
-        sb.AppendLine("        }");
-        sb.AppendLine();
-        sb.AppendLine("        foreach (var tag in Tags)");
-        sb.AppendLine("        {");
-        sb.AppendLine("            if (tag.Contains(value))");
-        sb.AppendLine("            {");
-        sb.AppendLine("                return true;");
-        sb.AppendLine("            }");
-        sb.AppendLine("        }");
-        sb.AppendLine();
-        sb.AppendLine("        return false;");
-        sb.AppendLine("    }");
+        //sb.AppendLine("namespace Lucide.Avalonia.Metadata;");
+        //sb.AppendLine();
+        //sb.AppendLine("public class LucideIconInfo");
+        //sb.AppendLine("{");
+        //sb.AppendLine("    public required LucideIconKind Kind { get; init; }");
+        //sb.AppendLine();
+        //sb.AppendLine("    public required string[] Contributors { get; init; }");
+        //sb.AppendLine();
+        //sb.AppendLine("    public required string[] Categories { get; init; }");
+        //sb.AppendLine();
+        //sb.AppendLine("    public required string[] Tags { get; init; }");
+        //sb.AppendLine();
+        //sb.AppendLine("    public bool Contains(string? value)");
+        //sb.AppendLine("    {");
+        //sb.AppendLine("        if (string.IsNullOrEmpty(value))");
+        //sb.AppendLine("        {");
+        //sb.AppendLine("            return true;");
+        //sb.AppendLine("        }");
+        //sb.AppendLine();
+        //sb.AppendLine("        value = value.ToLower();");
+        //sb.AppendLine();
+        //sb.AppendLine("        if (Kind.ToString().Contains(value, StringComparison.OrdinalIgnoreCase))");
+        //sb.AppendLine("        {");
+        //sb.AppendLine("            return true;");
+        //sb.AppendLine("        }");
+        //sb.AppendLine();
+        //sb.AppendLine("        foreach (var category in Categories)");
+        //sb.AppendLine("        {");
+        //sb.AppendLine("            if (category.Contains(value))");
+        //sb.AppendLine("            {");
+        //sb.AppendLine("                return true;");
+        //sb.AppendLine("            }");
+        //sb.AppendLine("        }");
+        //sb.AppendLine();
+        //sb.AppendLine("        foreach (var tag in Tags)");
+        //sb.AppendLine("        {");
+        //sb.AppendLine("            if (tag.Contains(value))");
+        //sb.AppendLine("            {");
+        //sb.AppendLine("                return true;");
+        //sb.AppendLine("            }");
+        //sb.AppendLine("        }");
+        //sb.AppendLine();
+        //sb.AppendLine("        return false;");
+        //sb.AppendLine("    }");
+
+        sb.AppendLine("""
+            namespace Lucide.Avalonia.Metadata;
+
+            public class LucideIconInfo
+            {
+                public required LucideIconKind Kind { get; init; }
+
+                public required string[] Contributors { get; init; }
+
+                public required string[] Categories { get; init; }
+
+                public required string[] Tags { get; init; }
+
+                public bool Contains(string? value, out int priority)
+                {
+                    priority = 0;
+
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        return true;
+                    }
+
+                    value = value.ToLower();
+
+                    var kind = Kind.ToString().ToLower();
+
+                    if (kind == value)
+                    {
+                        return true;
+                    }
+
+                    if (kind.StartsWith(value))
+                    {
+                        priority = 1;
+                        return true;
+                    }
+
+                    if (kind.Contains(value))
+                    {
+                        priority = 2;
+                        return true;
+                    }
+
+                    foreach (var category in Categories)
+                    {
+                        if (category.Contains(value))
+                        {
+                            priority = 3;
+                            return true;
+                        }
+                    }
+
+                    foreach (var tag in Tags)
+                    {
+                        if (tag.Contains(value))
+                        {
+                            priority = 4;
+                            return true;
+                        }
+                    }
+
+                    return false;
+                }
+            """);
+
         sb.AppendLine();
         sb.AppendLine("    public static LucideIconInfo GetIconInfo(LucideIconKind kind)");
         sb.AppendLine("    {");
