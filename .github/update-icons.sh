f456bb8ecc6cd2a8c28660495499de402eb5abd2
dme-compunet <117437050+dme-compunet@users.noreply.github.com>
2025-03-19 18:25:32 +0200

    Create auto release

--- .github/update-icons.sh
+++ .github/update-icons.sh
@@ -5,7 +5,9 @@ git add ./src/Lucide.Avalonia/LucideIconKind.cs
 git add ./src/Lucide.Avalonia/Metadata/LucideIconInfo.cs
 if [ -n "$(git diff --cached --name-only)" ]; then
     git commit -m "🔄 Update Icon Collection"
+    nu .github/create-tag.nu
     git push
+    git push --tags
 else
     echo "No changes to commit"
 fi
